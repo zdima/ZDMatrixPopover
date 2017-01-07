0134d54514b9503868a74652657ddfe390503088
Dmitriy Zakharkin <mail@zdima.net>
2017-01-07 14:27:31 -0500

    Initial Commit

---
+++ ZDMatrixPopover/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ZDMatrixPopover
+//
+//  Created by Dmitriy Zakharkin on 1/7/17.
+//  Copyright © 2017 Dmitriy Zakharkin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ZDMatrixPopover/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ZDMatrixPopover
+//
+//  Created by Dmitriy Zakharkin on 1/7/17.
+//  Copyright © 2017 Dmitriy Zakharkin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
